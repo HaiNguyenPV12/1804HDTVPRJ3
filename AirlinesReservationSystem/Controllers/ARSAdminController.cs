2cabb381a934d9a6ccdfcd81186f7f02b3be2ae1
Hai Thien <39566043+haithienht@users.noreply.github.com>
2019-07-23 20:47:09 +0700

    Admin-EmployeeList

--- AirlinesReservationSystem/Controllers/ARSAdminController.cs
+++ AirlinesReservationSystem/Controllers/ARSAdminController.cs
@@ -9,24 +9,96 @@ namespace AirlinesReservationSystem.Controllers
 {
     public class ARSAdminController : Controller
     {
-        
+
         // GET: ARSAdmin
         public ActionResult Index()
         {
-            if (true)
+            if (IsLoggedIn())
             {
+                return View();
+            }
+            return RedirectToAction("Login");
+        }
 
+        public ActionResult Route()
+        {
+            if (IsLoggedIn())
+            {
+                return View();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
+            if (!IsLoggedIn())
+            {
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Employee emp)
+        {
+            ModelState.Remove("Firstname");
+            ModelState.Remove("Lastname");
+            ModelState.Remove("Address");
+            ModelState.Remove("Phone");
+            ModelState.Remove("Email");
+            ModelState.Remove("Sex");
+            ModelState.Remove("DoB");
+            ModelState.Remove("IsActive");
+            ModelState.Remove("ROLE");
+            if (ModelState.IsValid)
+            {
+                var e = EmployeeDAO.CheckLogin(emp.EmpID, emp.Password);
+                if (e != null)
+                {
+                    Session["employee"] = e;
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("", "Invalid account!");
             }
             return View();
         }
-        public ActionResult Route()
+
+        public ActionResult Logout()
         {
-            return View();
+            Session["employee"] = null;
+            return RedirectToAction("Index");
         }
 
-        public int CheckLogin()
+        public ActionResult Employee()
         {
-            return 0;
+            if (IsAdminLoggedIn())
+            {
+                return View(EmployeeDAO.GetEmployeeList());
+            }
+            return RedirectToAction("Index");
+        }
+
+        public bool IsLoggedIn()
+        {
+            if (Session["employee"] != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsAdminLoggedIn()
+        {
+            Employee e = (Employee)Session["employee"];
+            if (e != null)
+            {
+                if (e.ROLE == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
---
+++ AirlinesReservationSystem/Models/EmployeeDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AirlinesReservationSystem.Models
+{
+    public class EmployeeDAO
+    {
+        static AirlineDBEntities db = new AirlineDBEntities();
+
+        public static Employee GetEmployee(string EmpID) { return db.Employee.FirstOrDefault(e => e.EmpID == EmpID); }
+
+        public static IEnumerable<Employee> GetEmployeeList() { return db.Employee; }
+
+        public static Employee CheckLogin(string EmpID, string Password)
+        {
+            return db.Employee.Where(e => e.EmpID.Trim() == EmpID && e.Password == Password).FirstOrDefault();
+        }
+    }
+
+}
