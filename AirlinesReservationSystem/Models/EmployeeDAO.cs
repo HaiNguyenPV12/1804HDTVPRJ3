46f4e12d5d304735829e6789d2ce3566a0a11493
Hai Thien <39566043+haithienht@users.noreply.github.com>
2019-07-24 15:44:37 +0700

    Admin-update Employee (Add+Delete+Modal)

--- AirlinesReservationSystem/Models/EmployeeDAO.cs
+++ AirlinesReservationSystem/Models/EmployeeDAO.cs
@@ -18,6 +18,49 @@ namespace AirlinesReservationSystem.Models
         {
             return db.Employee.Where(e => e.EmpID.Trim() == EmpID && e.Password == Password).FirstOrDefault();
         }
+
+        public static bool AddEmployee(Employee newE)
+        {
+            if (GetEmployee(newE.EmpID) == null)
+            {
+                db.Employee.Add(newE);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public static bool DeleteEmployee(string id)
+        {
+            var e = GetEmployee(id);
+            if (e != null)
+            {
+                db.Employee.Remove(e);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public static bool UpdateEmployee(Employee updateE)
+        {
+            var e = GetEmployee(updateE.EmpID);
+            if (e != null)
+            {
+                e.Password = updateE.Password;
+                e.Firstname = updateE.Firstname;
+                e.Lastname = updateE.Lastname;
+                e.Address = updateE.Address;
+                e.Phone = updateE.Phone;
+                e.Sex = updateE.Sex;
+                e.IsActive = updateE.IsActive;
+                e.DoB = updateE.DoB;
+                e.Email = updateE.Email;
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
     }
 
 }
