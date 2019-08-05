099bfbfef1cd4052bbacfd36c6ee41776ccfbb4b
HaiNguyenPV12 <39506270+HaiNguyenPV12@users.noreply.github.com>
2019-08-05 14:21:40 +0700

    404 page

---
+++ AirlinesReservationSystem/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AirlinesReservationSystem.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Error404()
+        {
+            return View();
+        }
+    }
+}
