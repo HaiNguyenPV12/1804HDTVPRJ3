fe03d80e538d9e9fa7c5c8fe7d078ee76ca2ea79
HaiNguyenPV12 <39506270+HaiNguyenPV12@users.noreply.github.com>
2019-08-08 10:21:03 +0700

    front page redesign

---
+++ AirlinesReservationSystem/Models/ars/AirlineDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirlinesReservationSystem.Models.ars
+{
+    public class AirlineDAO
+    {
+        private static AirlineDBEntities db = new AirlineDBEntities();
+
+        public static IEnumerable<Airline> GetAirlines() => db.Airline;
+    }
+}
---
+++ AirlinesReservationSystem/Scripts/ars/layout.js
@@ -0,0 +1,17 @@
+﻿
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    $("html, body").animate({ scrollTop: 0 }, 350);
+    return false;
+}
